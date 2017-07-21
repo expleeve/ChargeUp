dc8d9b48cbcd1254182a40fb1d8611178bbc97e8
expleeve <hnchenkun8801@163.com>
2017-07-22 01:45:43 +0800

    remove mybatis mapper

--- chargeup/src/main/java/com/expleeve/Chargeup.java
+++ chargeup/src/main/java/com/expleeve/Chargeup.java
@@ -1,19 +1,24 @@
 package com.expleeve;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
- * Hello world!
+ * Starter
  *
  */
 @EnableTransactionManagement
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages={"com.expleeve.chargeup"})
 public class Chargeup 
 {
     public static void main( String[] args )
     {
-    	SpringApplication.run(Chargeup.class, args);
+    	System.setProperty("spring.devtools.restart.enabled", "true");
+    	SpringApplication app = new SpringApplication(Chargeup.class);
+    	//disable banner
+    	app.setBannerMode(Banner.Mode.OFF);
+    	app.run(args);
     }
 }
